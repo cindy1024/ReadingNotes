4b987d1b24b67b27b9364392b6becac1414332cf
cindy_1024 <wxj_1024@163.com>
2020-03-03 11:07:40 +0800

    2020-03-03_11:07:30

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cur_date="`date '+%Y-%m-%d_%H:%M:%S'`"
+
+git checkout master
+
+gitbook build
+
+git add .
+git commit -m $cur_date
+git push -u origin master
+git checkout gh-pages
+cp -r _book/* .
+git add .
+git commit -m $cur_date
+git push -u origin gh-pages
+git checkout master
