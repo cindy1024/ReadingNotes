387736ebe8502312cb8e055982f16200a11f987f
cindy1024 <wxj_1024@163.com>
2020-08-17 17:55:16 +0800

    {}/{2020-08-17_17:55:15}

--- update.sh
+++ update.sh
@@ -6,11 +6,14 @@ git checkout master
 gitbook build
 
 git add .
-git commit -m $cur_date
+git commit -m {$1}/{$cur_date}
 git push -u origin master
 git checkout gh-pages
 cp -r _book/* .
 git add .
-git commit -m $cur_date
+git commit -m {$1}/{$cur_date}
 git push -u origin gh-pages
 git checkout master
+
+#usage
+#sh xxx.sh '更新说明'
