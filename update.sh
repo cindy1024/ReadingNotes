aaaf5b8ee0d59cc4e1ced439fabd9fa7e4003653
cindy1024 <wxj_1024@163.com>
2020-08-18 16:13:30 +0800

    /2020-08-18_16:13:25

--- update.sh
+++ update.sh
@@ -6,12 +6,12 @@ git checkout master
 gitbook build
 
 git add .
-git commit -m {$1}/{$cur_date}
+git commit -m $1/$cur_date
 git push -u origin master
 git checkout gh-pages
 cp -r _book/* .
 git add .
-git commit -m {$1}/{$cur_date}
+git commit -m $1/$cur_date
 git push -u origin gh-pages
 git checkout master
 
